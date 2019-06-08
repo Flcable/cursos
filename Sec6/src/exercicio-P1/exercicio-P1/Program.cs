87463cbba45c1a21ec00756bde7f91e63b2ca05b
Flcable <flcable@gmail.com>
2019-06-08 01:03:12 +0100

    Arrays

---
+++ Sec6/src/exercicio-P1/exercicio-P1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercicio_P1 {
+    class Program {
+        static void Main(string[] args) {
+
+            int n;
+            double[] vet;
+
+            n = int.Parse(Console.ReadLine());
+            vet = new double[n];
+            
+            string[] linha = Console.ReadLine().Split();
+            for (int i = 0; i < n; i++) {
+                vet[i] = double.Parse(linha[i]);
+            }
+
+            for (int i = 0; i < n; i++) {
+
+            }
+
+
+
+
+                Console.ReadLine();
+        }
+    }
+}
---
+++ Sec6/src/exercicio-p2/exercicio-p2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace exercicio_p2 {
+    class Program {
+        static void Main(string[] args) {
+
+            int N;
+            int[] vet;
+
+            N = int.Parse(Console.ReadLine());
+            vet = new int[N];
+
+            string[] linha = Console.ReadLine().Split(' ');
+            for (int i = 0; i > N; i++) {
+                vet[i] = int.Parse(linha[i]);
+            }
+
+            for (int i = 0; i > N; i++) {
+                if (vet[i] % 2 == 0) {
+                    Console.WriteLine(vet[i].ToString());
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
